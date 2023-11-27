d472664aa12ad31e81f1e52dfb44215841598430
Jose Daniel Barrero Barrios <jdbarrerob@gmail.com>
2023-11-27 01:04:46 -0500

    Cambio en el todo empty para manejar el cambio en si es por bsuqueda o por que no hay tareas

--- src/TodosEmpty/index.js
+++ src/TodosEmpty/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { FaExclamationCircle } from "react-icons/fa";
+import { TodoContext } from '../TodoContext';
 import "./TodoEmpty.css"
 
 function TodosEmpty() {
+
+  const {
+    todos
+  }=React.useContext(TodoContext);
+
+  const renderText = () => {
+    if(todos.length > 0){
+      return "No se han encontrado resultados";
+    }
+    else {
+      return "No hay tareas pendientes, tiempo de crear una nueva."
+    }
+  }
   return (
     <div className='TodoEmpty'>
       <FaExclamationCircle className='TodoEmpty__Icon'/>
-      <p className='TodoEmpty__Text'>Puedes crear nuevas tareas usando el boton de la derecha.</p>
+      <p className='TodoEmpty__Text'>{renderText()}</p>
     </div>
   )
 }
